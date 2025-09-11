623d4bff7f767348b6082b91ed636026ee790a9d
yorick <yorick.scheffler@student.hpi.de>
2025-09-11 12:35:47 +0200

    fix Left Right

--- classifier/rilewresponses.py
+++ classifier/rilewresponses.py
@@ -2,16 +2,16 @@ import numpy as np
 import pandas as pd
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from .shared_model_cache import SharedModelCache
 import warnings
 warnings.filterwarnings('ignore')
 
-class PopulismPluralismResponsesScorer:
+class LeftRightResponsesScorer:
     """Populism-Pluralism classification using rile (2020) framework"""
     
     def __init__(self, model_name="mlburnham/Political_DEBATE_large_v1.0"):
-        print(f"Loading Populism-Pluralism NLI model: {model_name}...")
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        cache = SharedModelCache()
+        self.model, self.tokenizer = cache.get_model_and_tokenizer(model_name)
         self.entailment_idx = self._find_entailment_index()
         
         self.topic_question = (
