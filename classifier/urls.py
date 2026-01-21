b69a673f79bc44e9a3b03a624d3de3804bd69921
Juan Gomezcruces <juan.gomezcruces@hpi.de>
2026-01-21 12:38:42 +0100

    add imprint

--- classifier/urls.py
+++ classifier/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('classify/', views.classify_text, name='classify_text'),
     path('api/classify/', views.api_classify, name='api_classify'),
-    path('privacy/', views.privacy_notice, name='privacy_notice'),  # NEW
+    path('privacy/', views.privacy_notice, name='privacy_notice'),
+    path('imprint/', views.imprint, name='imprint'),
 ]
