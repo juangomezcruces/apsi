05271621fb4dd8b5aa8ec6f2898bb948a75489ee
Juan Gomezcruces <juan.gomezcruces@hpi.de>
2026-02-02 15:05:36 +0100

    Edit settings.py

--- political_classifier/settings.py
+++ political_classifier/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key-here-change-in-production'
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'apsi.sc.hpi.de', 'political-classifier.cloud.sci.hpi.de']
 
 INSTALLED_APPS = [
