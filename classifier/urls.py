78337fa2f7b59bde4680202b4d0c3640c8273730
Juan Gomezcruces <juan.gomezcruces@hpi.de>
2026-02-02 14:49:25 +0100

    Edit urls.py

--- classifier/urls.py
+++ classifier/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 app_name = 'classifier'
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('classify/', views.classify_text, name='classify_text'),
     path('api/classify/', views.api_classify, name='api_classify'),
+    path('analysis/', views.analysis, name='analysis'), 
     path('privacy/', views.privacy_notice, name='privacy_notice'),
     path('imprint/', views.imprint, name='imprint'),
     path('contact/', views.contact, name='contact'),
