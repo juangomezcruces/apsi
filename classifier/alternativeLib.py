f5e1541996f2daf3126dce8dcc3613b8b4477f3b
juangomezcruces <69355286+juangomezcruces@users.noreply.github.com>
2026-04-07 17:17:09 +0200

    Update alternativeLib.py

--- classifier/alternativeLib.py
+++ classifier/alternativeLib.py
@@ -293,7 +293,7 @@ class LiberalIlliberalScorer:
             'threshold':                thr,
         }
 
-    def quick_score(self, text, thr=0.4):
+    def quick_score(self, text, thr=0.5):
         result = self.score_liberal_illiberal(text, thr=thr)
         return result['score']
 
